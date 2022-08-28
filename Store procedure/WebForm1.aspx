5bf9cbd1e47f1ddebddc807efdee3c826e3a8ae1
surender kumar <surenderks089@gmail.com>
2022-08-28 12:25:00 +0530

    Add project files.

---
+++ Store procedure/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Store_procedure.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <strong>Empno:</strong>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <strong>Ename:</strong>&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <strong>Eadd:&nbsp;&nbsp;&nbsp; </strong>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <strong>Esal:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <br />
+        </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="font-weight: 700" Text="SAVE" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" style="font-weight: 700" Text="UPDATE" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" style="font-weight: 700" Text="DELETE" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" style="font-weight: 700" Text="DISPLAY" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Width="433px"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ Store procedure/WebForm1.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Store_procedure
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            if (Page.IsPostBack == false)
+            {
+                disp_Rec();
+            }
+        }    
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "insemp";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@eno", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@en", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@ed", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@es", TextBox4.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+
+            clear_Rec();
+        }
+
+        private void clear_Rec()
+        {
+            TextBox1.Text = string.Empty;
+            TextBox2.Text = string.Empty;
+            TextBox3.Text = string.Empty;
+            TextBox4.Text = string.Empty;
+            TextBox1.Focus();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            disp_Rec();
+
+        }
+
+        private void disp_Rec()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "disemp";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+            SqlDataReader dr = cmd.ExecuteReader();
+            ListBox1.DataTextField = "ename";
+            ListBox1.DataValueField = "empno";
+            ListBox1.DataSource = dr;
+            ListBox1.DataBind();
+            dr.Close();
+            cmd.Dispose();
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "findemp";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@eno", ListBox1.SelectedValue);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if(dr.HasRows)
+            {
+                dr.Read();
+            }
+            TextBox1.Text = dr[0].ToString();
+            TextBox2.Text = dr[1].ToString();
+            TextBox3.Text = dr[2].ToString();
+            TextBox4.Text = dr[3].ToString();
+            dr.Close();
+            cmd.Dispose();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "updemp";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@eno", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@en", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@ed", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@es", TextBox4.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            clear_Rec();
+            disp_Rec();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "delemp";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@eno", TextBox1.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            disp_Rec();
+            clear_Rec();
+
+        }
+    }
+}
